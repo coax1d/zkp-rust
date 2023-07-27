0756e481e4f4aa781426799293a50f37975e72cd
Andrew Burger <andrewburger1991@gmail.com>
2023-07-27 22:14:46 +0200

    first working impl of a zk-snark hand crafted

--- zk-snark/src/main.rs
+++ zk-snark/src/main.rs
@@ -3,33 +3,67 @@
 //! The proof is then verified to ensure that it is valid.
 //! Statement of the problem: Given two numbers `a` and `b`, prove that `a + b = 42`.
 
-use pairing::{
-    Engine,
-};
+use pairing::{Engine, MultiMillerLoop};
 use bls12_381::{Bls12, Scalar};
 use rand::rngs::OsRng;
 use bellman::{
+    gadgets::num::AllocatedNum,
     groth16::{
-        create_random_proof, generate_random_parameters, prepare_verifying_key,
+        create_random_proof, generate_random_parameters, prepare_verifying_key, self,
     },
     Circuit, ConstraintSystem, SynthesisError, Variable,
 };
-use blake2::Blake2s;
+
 use ff::PrimeField;
 
-// This is a simple circuit with two values.
-struct ZkSnarkCircuit {
-    a: Option<Scalar>,
-    b: Option<Scalar>,
+// Circuit y = x^2 + a
+// Where:
+// x, a are private and y is public
+struct FirstCircuit<S> {
+    pub a: Option<S>,
+    pub x: Option<S>,
 }
 
-impl<S: PrimeField> Circuit<S> for ZkSnarkCircuit {
+impl<S: PrimeField> Circuit<S> for FirstCircuit<S> {
     fn synthesize<CS: ConstraintSystem<S>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
-        // Allocate the first number as a private input to the circuit.
-        // let a = cs.alloc(|| "a", || self.a.ok_or(SynthesisError::AssignmentMissing))?; // a = 1
-        // println!("a is after alloc: {:?}", a);
-        // let b = cs.alloc(|| "b", || self.b.ok_or(SynthesisError::AssignmentMissing))?; // b = 2
-        // println!("b is after alloc: {:?}", b);
+
+        // allocate private variable x
+        let x = cs.alloc(|| "x", || self.x.ok_or(SynthesisError::AssignmentMissing))?;
+
+        // Save result of x^2 for later
+        let mut x_squared_scalar = S::from(0);
+        let x_squared = cs.alloc(|| "x_squared", ||{
+            let tmp_x = self.x.ok_or(SynthesisError::AssignmentMissing)?;
+            let x_squared = tmp_x * tmp_x;
+            x_squared_scalar = x_squared;
+            Ok(x_squared)
+        })?;
+
+        cs.enforce(
+            || "x * x = x^2",
+            |lc| lc + x,
+            |lc| lc + x,
+            |lc| lc + x_squared
+        );
+
+        // allocate other private variable a
+        let a = cs.alloc(|| "a", || self.a.ok_or(SynthesisError::AssignmentMissing))?;
+
+        // allocate public variable y
+        let y = cs.alloc_input(|| "y", || {
+            let a = self.a.ok_or(SynthesisError::AssignmentMissing)?;
+            let x = self.x.ok_or(SynthesisError::AssignmentMissing)?;
+            let y_scalar = a + x_squared_scalar;
+            Ok(y_scalar)
+        })?;
+
+        cs.enforce(
+            || "y = x^2 + a",
+            |lc| lc + a + x_squared,
+            |lc| lc + CS::one(),
+            |lc| lc + y
+        );
+
         Ok(())
     }
 }
@@ -39,12 +73,22 @@ impl<S: PrimeField> Circuit<S> for ZkSnarkCircuit {
 fn main() {
     let rng = &mut OsRng;
 
-    // Create parameters for our circuit.
-    // let params = {
-    //     let c = ZkSnarkCircuit {
-    //         a: Some(Scalar::from(30u64)),
-    //         b: Some(Scalar::from(12u64)),
-    //     };
-    //     generate_random_parameters::<Bls12, _, _>(c, rng).unwrap()
-    // };
+    // Create some random params for setup
+    let params = {
+        let c = FirstCircuit { a: None, x: None };
+        groth16::generate_random_parameters::<Bls12, _, _>(c, rng).unwrap()
+    };
+
+    // prep a verification key
+    let pvk = groth16::prepare_verifying_key(&params.vk);
+
+    let my_circuit = FirstCircuit { a: Some(Scalar::from(3)), x: Some(Scalar::from(5)) };
+
+    // Create Groth16 proof with custom params
+    let proof = groth16::create_random_proof(my_circuit, &params, rng).unwrap();
+
+    let solution = (5 * 5) + 3;
+    let y = Scalar::from(solution);
+
+    assert!(groth16::verify_proof(&pvk, &proof, &[y]).is_ok());
 }
